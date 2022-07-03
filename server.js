43010e80f84659327a4a06106019ac89b97f80ee
end <72604018+laok233@users.noreply.github.com>
2022-07-03 16:42:00 +0200

    update server.js

--- server.js
+++ server.js
@@ -22,26 +22,35 @@ app.get("/users/:id", (req, res) => {
 	});
 });
 
-app.post("/login", function (req, res) {
-	const username = req.body.username;
-	const password = req.body.password;
-
-	const mockUsername = "billyTheKid";
-	const mockPassword = "superSecret";
-
-	if (username === mockUsername && password === mockPassword) {
-		res.json({
-			success: true,
-			message: "password and username match!",
-			token: "encrypted token goes here",
-		});
-	} else {
-		res.json({
-			success: false,
-			message: "password and username do not match",
-		});
-	}
+app.get("/time", (req, res) => {
+    const date = new Date()
+	res.json({
+		success: true,
+		message: "time",
+		date: date,
+	});
 });
+
+app.post("/login", (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        const mockUsername = "billyTheKid";
+        const mockPassword = "superSecret";
+
+        if (username === mockUsername && password === mockPassword) {
+            res.json({
+                success: true,
+                message: "password and username match!",
+                token: "encrypted token goes here",
+            });
+        } else {
+            res.json({
+                success: false,
+                message: "password and username do not match",
+            });
+        }
+    });
 
 app.listen(8000, () => {
 	console.log("server is running");
